6f56f3dcaef3961d40962da6dc78d3ab2614cd91
Kosalram Rama Krishnan <kosalram@ideas2it.com>
2019-01-09 20:05:58 +0530

    Added models

--- src/main.go
+++ src/main.go
@@ -5,24 +5,31 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"src/models"
+
+	_ "github.com/lib/pq"
 )
 
 /*Address is a derived datatype that gets address data of a user and constructs a JSON object */
 type Address struct {
-	ID       int    `json: "id"`
-	Location string `json: "location"`
+	ID       int    `json:"id"`
+	Location string `json:"location"`
 }
 
 /*User is a derived datatype that gets user data and constructs a JSON object */
 type User struct {
-	ID            int       `json: "id"`
-	Name          string    `json: "name"`
-	Mobile_Number string    `json: "mobile_number"`
-	Email         string    `json: "email"`
-	Addresses     []Address `json: "addresses"`
+	ID           int       `json:"id"`
+	Name         string    `json:"name"`
+	MobileNumber string    `json:"mobile_number"`
+	Email        string    `json:"email"`
+	Addresses    []Address `json:"addresses"`
 }
 
 func main() {
+	connectionString := "user=postgres dbname=learnGO  password=ubuntu"
+	driverName := "postgres"
+	models.InitDB(driverName, connectionString)
 	fmt.Println("Happy Learning.....!!!!!!")
 
 	jsonFile, err := os.Open("data.json")
